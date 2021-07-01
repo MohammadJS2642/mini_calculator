4cd30e8ae8549a69203e0f5203caf8f4a7e6077e
MohammadJS2642 <mohammadjs2642@gmail.com>
2021-07-01 11:33:12 +0430

    display is OK

--- components/display/display.js
+++ components/display/display.js
@@ -16,7 +16,6 @@ export default function Showing({ value, style }) {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
         justifyContent: 'center',
         paddingRight: 20,
     },
