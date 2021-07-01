b4123ebdac98c6a48ec29e90ba52f788c9f4e89f
MohammadJS2642 <mohammadjs2642@gmail.com>
2021-07-01 11:31:55 +0430

    buttons position is OK

--- components/myButton/buttonsPosition.js
+++ components/myButton/buttonsPosition.js
@@ -1,40 +1,39 @@
 import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import MyButton from './customButton';
-import Showing from '../display/display';
 
 export default function ButtonsPosition({ style }) {
     return (
         <View style={[styles.container, style]}>
-            <View style={styles.co}>
-                <View style={styles.containerItem}>
-                    <MyButton name={'0'} size='md' />
-                    <MyButton name={'.'} />
-                    <MyButton name={'='} />
-                </View>
-                <View style={styles.containerItem}>
-                    <MyButton name={'1'} />
-                    <MyButton name={'2'} />
-                    <MyButton name={'3'} />
-                    <MyButton name={'+'} />
-                </View>
-                <View style={styles.containerItem}>
-                    <MyButton name={'4'} />
-                    <MyButton name={'5'} />
-                    <MyButton name={'6'} />
-                    <MyButton name={'-'} />
-                </View>
-                <View style={[styles.containerItem]}>
-                    <MyButton name={'7'} />
-                    <MyButton name={'8'} />
-                    <MyButton name={'9'} />
-                    <MyButton name={'*'} />
-                </View>
+            <View style={styles.containerItem}>
+                <MyButton name={'0'} size='md' />
+                <MyButton name={'.'} />
+                <MyButton name={'='} bgColor='gr' />
             </View>
-            <View>
-                <Showing style={styles.showingStyle} />
+            <View style={styles.containerItem}>
+                <MyButton name={'1'} />
+                <MyButton name={'2'} />
+                <MyButton name={'3'} />
+                <MyButton name={'+'} bgColor='gr' />
             </View>
-
+            <View style={styles.containerItem}>
+                <MyButton name={'4'} />
+                <MyButton name={'5'} />
+                <MyButton name={'6'} />
+                <MyButton name={'-'} bgColor='gr' />
+            </View>
+            <View style={[styles.containerItem]}>
+                <MyButton name={'7'} />
+                <MyButton name={'8'} />
+                <MyButton name={'9'} />
+                <MyButton name={'*'} bgColor='gr' />
+            </View>
+            <View style={[styles.containerItem]}>
+                <MyButton name={'AC'} bgColor='gr' />
+                <MyButton name={'C'} bgColor='gr' />
+                <MyButton name={'%'} bgColor='gr' />
+                <MyButton name={'/'} bgColor='gr' />
+            </View>
         </View>
     );
 }
@@ -47,17 +46,10 @@ const styles = StyleSheet.create({
         flexDirection: 'column-reverse',
 
     },
-    co: {
-        flex: 1,
-    },
     containerItem: {
         paddingHorizontal: 30,
         paddingVertical: 20,
         justifyContent: 'space-between',
         flexDirection: 'row',
-    },
-    showingStyle: {
-        flex: 1,
-        backgroundColor: '#B9B7BD',
     },
 })
