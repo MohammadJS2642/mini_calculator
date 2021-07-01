1a8ccd94ce234e65628e1a814039838ef6305fab
MohammadJS2642 <mohammadjs2642@gmail.com>
2021-07-01 11:35:07 +0430

    Share.js component is showing

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import ButtonsPosition from './components/myButton/buttonsPosition'
+import Share from './components/share'
 
 export default function App() {
   return (
-    <ButtonsPosition />
+    <Share />
   );
 }
 
