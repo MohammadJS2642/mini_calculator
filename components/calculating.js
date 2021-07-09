6afda2e1691acfe745cbe0dd1de655e1b1c335ce
MohammadJS2642 <mohammadjs2642@gmail.com>
2021-07-09 22:59:48 +0430

    ( + - * /) is now work with calculating

--- components/calculating.js
+++ components/calculating.js
@@ -1,30 +1,100 @@
-import { Number2, Numbers } from "./data";
-import React from 'react';
-
 export const Data = {
     currentValue: '0',
-    PreviousValue: null,
+    PreviousValue: '0',
     operator: null,
 };
 
-const checkingID = (userID) => {
-    Number2.forEach(index => {
-        if (userID === index.id) {
-            // Data.PreviousValue = Data.currentValue;
-            if (Data.currentValue.length != 8) {
-                if (Data.currentValue === '0') {
-                    Data.currentValue = index.name.toString();
-                }
-                else {
-                    Data.currentValue += index.name.toString();
-                }
-            }
+// const checkingID = (userID) => {
+//     Number2.forEach(index => {
+//         if (userID === index.id) {
+//             // Data.PreviousValue = Data.currentValue;
+//             if (Data.currentValue.length != 8) {
+//                 if (Data.currentValue === '0') {
+//                     Data.currentValue = index.name.toString();
+//                 }
+//                 else {
+//                     Data.currentValue += index.name.toString();
+//                 }
+//             }
+//         }
+//         return Data;
+//     })
+// };
+
+const checkingvalue = (value) => {
+    if (Data.currentValue.length != 8) {
+        if (Data.currentValue === '0') {
+            Data.currentValue = value.toString();
         }
-        return Data;
-    })
+        else {
+            Data.currentValue += value.toString();
+        }
+    }
+
+    return Data;
 };
 
+const calculator = (value) => {
+    let previouseNumber = parseFloat(Data.PreviousValue);
+    switch (value) {
+        case "+":
+            previouseNumber += parseFloat(Data.currentValue);
+            Data.operator = value;
+            Data.currentValue = '0'
+            Data.PreviousValue = previouseNumber
 
-export function Calculating(id) {
-    checkingID(id)
+            break;
+
+        case "-":
+            previouseNumber == 0 ?
+                previouseNumber = parseFloat(Data.currentValue) : previouseNumber -= parseFloat(Data.currentValue)
+            Data.operator = value;
+            Data.currentValue = '0'
+            Data.PreviousValue = previouseNumber
+            break;
+
+        case "*":
+            previouseNumber == 0 ?
+                previouseNumber = parseFloat(Data.currentValue) : previouseNumber *= parseFloat(Data.currentValue)
+            Data.operator = value;
+            Data.currentValue = '0'
+            Data.PreviousValue = previouseNumber
+            break;
+
+        case "/":
+            previouseNumber == 0 ?
+                previouseNumber = parseFloat(Data.currentValue) : previouseNumber /= parseFloat(Data.currentValue)
+            Data.operator = value;
+            Data.currentValue = '0'
+            Data.PreviousValue = previouseNumber
+            break;
+
+        case "AC":
+            Data.currentValue = '0';
+            Data.PreviousValue = '0';
+            Data.operator = null;
+            break;
+
+        case "C":
+            Data.currentValue = '0';
+            break;
+
+        case "=":
+            Data.currentValue = previouseNumber;
+            Data.operator = null;
+            Data.PreviousValue = '0';
+            break;
+
+        default:
+            checkingvalue(value);
+            break;
+    }
+
+    console.log(Data);
+    return Data;
+}
+
+
+export function Calculating(value) {
+    calculator(value)
 }
