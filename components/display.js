2946adad5e0f27093ff6d311319aef7a13c9478a
MohammadJS2642 <mohammadjs2642@gmail.com>
2021-07-11 22:14:25 +0430

    getVals props is now using

--- components/display.js
+++ components/display.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
-import { Data } from './calculating';
 
 export default function Display({ style, getVals }) {
+
     return (
         <View style={[styles.displayContainrt, style]}>
             <Text style={styles.textDisplay}>{getVals}</Text>
